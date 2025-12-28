cf170b4be29a332c7f3d423d297a7aee796059e9
sofiaserapio <sofia.serapio1@estudiant.uib.cat>
2025-12-28 17:24:18 +0100

    he creado la clase leer fichero con los metodos que se encargan de leer la lista de letras para meterlas en una array y de leer el dicionario para mirar si la palabra existe, escribir fichero y jugador. en la clase letras he creado el metodo que elije de manera random las letras de la array y el metodo que comprueba si la pabra es posible de crear con las letras correspondientes

--- src/practicafinalcifrasyletras/Jugador.java
+++ src/practicafinalcifrasyletras/Jugador.java
@@ -3,61 +3,12 @@
  */
 package practicafinalcifrasyletras;
 
+
 public class Jugador {
-
-    static LT lt = new LT();
-    static Cifras c = new Cifras();
-    static Letras l = new Letras();
-    static Estadisticas e = new Estadisticas();
-
-    public static void main(String[] args) {
-        menu();
-    }
-
-    public static void menu() {
-        int opcion;
-
-        System.out.println(
-                "Elige una opcion:\n"
-                + "1. Cifras\n"
-                + "2. Letra\n"
-                + "3. Salir\n"
-                + "4. Estadisticas"
-        );
-
-        try {
-            opcion = lt.llegirEnter(); 
-        } catch (Exception e) {
-            System.out.println("No se permiten letras. Introduce una opcion numerica.");
-            menu();
-            return; 
-        }
-
-        switch (opcion) {
-
-            case 1:
-                c.menuCifras();
-                break;
-
-            case 2:
-                l.menuLetras();
-                break;
-
-            case 3:
-                System.out.println("GRACIAS POR JUGAR!!!!"
-                        + "FINALIZANDO...");
-                break;
-
-            case 4:
-                e.LeerEstadisticas();
-                break;
-
-            default:
-                System.out.println("Esta opcion no existe");
-                menu();
-                break;
-        }
-
-    }
+    
+    
+    
+    
+    
+    
 }
-        
